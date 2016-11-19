4dd416af22b21dc143905f6354816848eefa81e6
Ronaldo Fuzinato <ronaldofuzinato123@gmail.com>
2016-11-18 22:56:08 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## dubtrack-autofollow
+
+A script for [dubtrack.fm](https://www.dubtrack.fm) to verify each 10 minutes if there's users, in the room you are in, you didn't followed yet and to follow them.
+
+### Installation Instructions
+
+1. Install a "user scripts" plug-in here:
+    1. Chrome: **[Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)**
+    1. Firefox: **[Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/)**
+1. Click **[dubtrack-autofollow.user.js](https://github.com/ronaldojf/dubtrack-autofollow/raw/master/dubtrack-autofollow.user.js)** and click "Install".
+    1. Note: If this doesn't show an installation screen, you can manually set this up by going into the plug-in options, click "Create new script" and then copy/paste the entire contents of the file above.
+1. Visit [dubtrack.fm](https://www.dubtrack.fm) and enter a room.
+1. Make sure Tampermonkey or Greasemonkey is enabled (e.g. click the Tampermonkey icon and select "Enabled").
+
+### Update Instructions
+
+Updating is easy. In Tampermonkey:
+
+1. Click the Tampermonkey icon (square with two dots).
+1. Click "Check for userscript updates".
+1. On the next screen, confirm that you want to update.
+
+![Update Screenshot](https://rawgit.com/ronaldojf/dubtrack-autofollow/master/images/update.png)
---
+++ dubtrack-autofollow.user.js
@@ -0,0 +1,62 @@
+/*
+ * dubtrack-autofollow
+ *
+ * For more information and installation instructions, please see:
+ * https://github.com/ronaldojf/dubtrack-autofollow/
+*/
+
+// ==UserScript==
+// @name         dubtrack-autofollow
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Automatically checks if there's users in the current room to follow.
+// @author       Ronaldo Fuzinato
+// @site         https://github.com/ronaldojf/dubtrack-autofollow/
+// @match        *://dubtrack.fm/*
+// @match        *://www.dubtrack.fm/*
+// @grant        none
+// @updateURL    https://github.com/ronaldojf/dubtrack-autofollow/raw/master/dubtrack-autofollow.user.js
+// ==/UserScript==
+
+(function($, console, Dubtrack) {
+  'use strict';
+
+  $(function() {
+    var followUsersRecursively = function(idsCollection, currentIndex) {
+      currentIndex = currentIndex || 0;
+
+      if (idsCollection.length > 0 && currentIndex <= (idsCollection.length - 1)) {
+        var followingUrl = Dubtrack.config.apiUrl + Dubtrack.config.urls.userFollowing.replace(':id', idsCollection[currentIndex]);
+
+        Dubtrack.helpers.sendRequest(followingUrl, {}, 'post', function() {
+          followUsersRecursively(idsCollection, currentIndex + 1);
+        });
+      }
+    };
+
+    var refreshFollows = function() {
+      if (Dubtrack.session.id && ((Dubtrack.room || {}).users || {}).rt_users) {
+        var followUrl = Dubtrack.config.apiUrl + Dubtrack.config.urls.userFollow.replace(':id', Dubtrack.session.id);
+
+        Dubtrack.helpers.sendRequest(followUrl, {}, 'get', function(err, response) {
+          var toFollow = [];
+          var users = Dubtrack.room.users.rt_users;
+          var follows = response.data.map(function(follow) { return follow.following; });
+
+          for (var i = 0; i < users.length; i++) {
+            if (follows.indexOf(users[i]) === -1) {
+              toFollow.push(users[i]);
+            }
+          }
+
+          console.log('dubtrack-autofollow: Going to follow ' + toFollow.length + ' users now! You already have ' + follows.length + ' follows.');
+          followUsersRecursively(toFollow);
+        });
+      }
+    };
+
+    setInterval(refreshFollows, 10 * 60 * 1000);
+    setTimeout(refreshFollows, 15 * 1000);
+  });
+
+})($, console, Dubtrack);
